73d04f4678a474d73160ff4a2680217b152ade16
Harris Ransom <hransom528@gmail.com>
2024-09-20 01:20:10 -0400

    Completed working version of Graph class for Random Walk

--- RandomWalk/Graph.py
+++ RandomWalk/Graph.py
@@ -5,7 +5,6 @@
 # Imports
 import numpy as np
 import pandas as pd
-import networkx as nx
 
 # Graph class
 class Graph:
@@ -15,24 +14,43 @@ class Graph:
 		#self.edgeList = []
 		self.nodes = numNodes
 		self.edges = numEdges
+		# TODO: Randomly assign edges in A
 		self.A = np.zeros((numNodes, numNodes))
 
-	# TODO: Create CSV import function
-	def importCSV(self, filename):
+	# String constructor
+	def __str__(self):
+		return np.array2string(self.A)
+
+	# CSV import function
+	@staticmethod
+	def importCSV(filename):
 		# Read the CSV file
-		df = pd.read_csv(filename)
+		df = pd.read_csv(filename, header=None)
+
+		# Create new graph object
+		self = Graph()
 
 		# Get the number of nodes
 		if (df.shape[0] != df.shape[1]):
 			raise ValueError("Adjacency matrix not square!")
 		self.nodes = df.shape[0]
+
+		# Initialize adjacency matrix from dataframe
+		self.A = df.to_numpy()
+
+		# Get the number of edges
+		self.edges = 0
+		for i in np.nditer(self.A):
+			if (i > 0):
+				self.edges += 1
 		
 		# Convert the DataFrame to a numpy array
 		self.A = df.to_numpy()
+		return self
 
 	# Export adjacency matrix to CSV
 	def exportCSV(self, filename):
-		pd.DataFrame(self.A).to_csv(filename, index=False)
+		pd.DataFrame(self.A).to_csv(filename, index=False, header=None)
 
 	# Getter method for set of node neighbors
 	def getNeighborSet(self, node):
@@ -60,11 +78,47 @@ class Graph:
 		
 		# Insert the row into the adjacency matrix
 		self.A = np.insert(self.A, index, row, axis=1)
-		
+		return self.A
+
 	# Replaces a row in the adjacency matrix at index with new row data
 	def replaceRow(self, row, index):
-		pass
+		# Check input row
+		if type(row) is list:
+			row = np.asarray(row)
+		if (row.size != self.nodes):
+			raise ValueError("Row size does not match number of nodes")
+		if (row.shape == (1, self.nodes)):
+			row = row.T
 
+		# Check input index
+		if (index < 0 or index >= self.nodes):
+			raise ValueError("Index out of bounds")
+		
+		# Replace row of adjacency matrix
+		self.A[index, :] = row
+		return self.A
+
+	# Check if graph is directed or undirected
+	def isDirected(self):
+		a, b = self.A.shape
+		if a == b and (np.transpose(self.A) == self.A).all(): # Symmetric matrix (undirected)
+			return False
+		return True
+	
+	# Check if graph is a Directed Acyclic Graph (DAG)
+	def isDag(self):
+		# Check if graph is directed
+		isDirected = self.isDirected()
+		if (not isDirected):
+			return False
+
+		# Check diagonals for cycles
+		diagonals = np.array(self.A.diagonal()).flatten()
+		for i in diagonals:
+			if (i == 1):
+				return False
+			
+		return True
 
 	# Gets the stationary distribution of the graph
 	def getStationaryDistribution(self):
--- RandomWalk/graphTest.py
+++ RandomWalk/graphTest.py
@@ -9,15 +9,33 @@ from Graph import Graph
 
 # MAIN
 if __name__ == "__main__":
-	# TODO: Get input
-	pass
+	# Get input
+	#numNodes = int(input("Enter number of nodes in graph: "))
+	#numEdges = int(input("Enter number of edges in graph: "))
+
 	# Create a graph
+	#graph1 = Graph(numNodes, numEdges)
 
 	# Test importCSV
-	
-	# Test insertRow
+	graph2 = Graph.importCSV("importCSV.csv")
 
 	# Test getStationaryDistribution
+	stationaryDist = graph2.getStationaryDistribution()
+
+	# Test replaceRow
+	replacementRow = [1,1,0,0,1]
+	Graph.replaceRow(graph2, replacementRow, 0)
+
+	# Test isDirected
+	isDirected = graph2.isDirected()
+	print(f"Imported graph directed? {isDirected}")
+
+	# Test isDag
+	isDag = graph2.isDag()
+	print(f"Is imported graph a DAG? {isDag}")
 
 	# Test exportCSV
+	graph2.exportCSV("exportCSV.csv")
 
+	# Output final graph (test __str__)
+	print(graph2)
