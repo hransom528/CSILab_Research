bc94b6a8b1ee0354b688d3dda210a2c7dfc0b44a
Harris Ransom <hransom528@gmail.com>
2025-03-13 00:57:30 -0400

    Added d-regular graph generation

--- RandomWalk/graphGen.py
+++ RandomWalk/graphGen.py
@@ -157,6 +157,47 @@ def erdosRenyiGraphGen(size, p, path="graphData/generatedErdosRenyiGraph.csv", p
 	# Return custom graph object
 	return GraphObj
 
+# Generates a single d-regular graph with specified size
+def dRegularGraphGen(size, d, path="graphData/generatedRegularGraph.csv", plotGraph=False):
+	G = nx.random_regular_graph(d, size)
+
+	# Sets node types
+	node_types = [-1] * (size)
+	color_map = ['blue'] * (size)
+	for i in range(0, size // 2):
+		node_types[i] = 1
+		color_map[i] = 'red'
+
+	# Plot graph if configured
+	graphLayout = nx.spring_layout(G)
+	if (plotGraph):
+		nx.draw(G, pos=graphLayout, node_color=color_map, with_labels=False, node_size=40)
+		plt.show()
+
+	# Export adjacency matrix to CSV
+	adjMatrix = nx.adjacency_matrix(G).toarray()
+	for i in range(0, size):
+		for j in range(0, size):
+			if (adjMatrix[i, j] != 0):
+				adjMatrix[i, j] = (node_types[i] * node_types[j])
+	graph_df = pd.DataFrame(adjMatrix)
+	graph_df.to_csv(path, header=False, index=False)
+
+	# Create custom Graph object
+	GraphObj = Graph.importTypedCSV(path, node_types)
+	GraphObj.layout = graphLayout
+	GraphObj.edges = G.number_of_edges()
+
+	# Export node types to file
+	with open(path+".types", "w") as output:
+		outStr = str(node_types)
+		outStr = outStr.replace("[", "")
+		outStr = outStr.replace("]", "")
+		output.write(outStr)
+
+	# Return custom graph object
+	return GraphObj
+
 # MAIN
 if __name__ == "__main__":
 	# Handles command-line arguments
