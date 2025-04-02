570c2c6b0c6fc2087f3a934629372f106efac669
Harris Ransom <hransom528@gmail.com>
2025-04-02 12:47:36 -0400

    Working on formatting Iris dataset

--- RandomWalk/IrisData.py
+++ RandomWalk/IrisData.py
@@ -34,6 +34,41 @@ X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=2)
 
-# TODO: Visualize data
+# Visualize Iris data
+def visualizeDataset():
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
+    for target, target_name in enumerate(names):
+        X_plot = X[y == target]
+        ax1.plot(X_plot[:, 0], X_plot[:, 1], 
+                linestyle='none', 
+                marker='o', 
+                label=target_name)
+    ax1.set_xlabel(feature_names[0])
+    ax1.set_ylabel(feature_names[1])
+    ax1.axis('equal')
+    ax1.legend()
+
+    for target, target_name in enumerate(names):
+        X_plot = X[y == target]
+        ax2.plot(X_plot[:, 2], X_plot[:, 3], 
+                linestyle='none', 
+                marker='o', 
+                label=target_name)
+    ax2.set_xlabel(feature_names[2])
+    ax2.set_ylabel(feature_names[3])
+    ax2.axis('equal')
+    ax2.legend()
+    plt.show()
+visualizeDataset()
 
 # TODO: Format data into PyTorch dataset/dataloader
+X_train = torch.Tensor(torch.from_numpy(X_train)).float()
+y_train = torch.Tensor(torch.from_numpy(y_train)).long()
+X_test  = torch.Tensor(torch.from_numpy(X_test)).float()
+y_test  = torch.Tensor(torch.from_numpy(y_test)).long()
+
+print(f"X_train dimensions: {X_train.size()}")
+print(f"Y_train dimensions: {y_train.size()}")
+print(f"X_test dimensions: {X_test.size()}")
+print(f"Y_test diemsnions: {y_test.size()}")
+print(y_train)
