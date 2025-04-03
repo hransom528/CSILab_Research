646aa9324f26998915ad3521d3447ea1db404c97
Harris Ransom <hransom528@gmail.com>
2025-04-02 23:51:27 -0400

    Added M-ary graph generation

--- RandomWalk/graphGen.py
+++ RandomWalk/graphGen.py
@@ -10,6 +10,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from Graph import Graph
 
+# Helper function to get m-ary colormaps
+'''
+def get_cmap(n, name='hsv'):
+    # Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    # RGB color; the keyword argument name must be a standard mpl colormap name.
+    return plt.cm.get_cmap(name, n)'
+'''
+
 # Graph Generation Function
 def graphGen(cluster_size=20, sparse_connections=3, p=0.5, path="graphData/generatedDisjointGraph.csv", plotGraph=False):
 	# Create two disjoint complete graphs and join them
@@ -74,6 +82,89 @@ def graphGen(cluster_size=20, sparse_connections=3, p=0.5, path="graphData/gener
 
 	# Return custom graph object
 	return GraphObj
+
+# Graph Generation for M-ary Erdos-Renyi clustered graph
+def mAryGraphGen(m=3, cluster_size=20, sparse_connections=3, p=0.5, path="graphData/generatedClusteredGraph.csv", plotGraph=False):
+	# Check m parameter
+	if (m <= 1):
+		raise ValueError(f"m is too small ({m}), needs to be at least 2!")
+	elif (m == 2):
+		G = graphGen(cluster_size, sparse_connections, p, path, plotGraph)
+		return G
+
+	# Create m disjoint clusters and join them
+	G = nx.Graph()
+	for i in range(m):
+		Gi = nx.erdos_renyi_graph(cluster_size, p)
+		G = nx.disjoint_union(G, Gi)
+	
+	# Set cluster types
+	node_types = []
+	for i in range(m):
+		type_val = i+1
+		cluster_types = [type_val] * cluster_size
+		for t in cluster_types:
+			node_types.append(t)
+
+	# Create sparse connections between each combinational pair of clusters
+	for i in range(m):
+		for j in range(i, m):
+			if (i != j):
+				for k in range(sparse_connections):
+					# Randomly select a node from the ith cluster
+					n1 = np.random.choice(np.arange(i * cluster_size, (i*cluster_size)+cluster_size))
+
+					# Randomly select a node form the jth cluster
+					n2 = np.random.choice(np.arange(j * cluster_size, (j*cluster_size)+cluster_size))
+
+					# Add edge
+					#print(f"({n1}, {n2})")
+					G.add_edge(n1, n2)
+
+	# Check for nodes with no connections
+	for i in range(len(G.nodes)):
+		if G.degree[i] == 0:
+			G.add_edge(i, np.random.choice(np.arange(G.nodes())))
+	
+	# Output joined graph
+	graphLayout = nx.spring_layout(G)
+	if (plotGraph):
+		if (m > 9):
+			raise ValueError("m-ary graph too big to plot (not enough colors)")
+		else:
+			# Set color map based on types
+			colors = "rbgcmykw"
+			color_map = []
+			for i in range(m):
+				cluster_color_type = colors[i]
+				cluster_colors = [cluster_color_type] * cluster_size
+				for c in cluster_colors:
+					color_map.append(c)
+
+			# Plot graph
+			nx.draw(G, pos=graphLayout, node_color=color_map, with_labels=False, node_size=40)
+			plt.show()
+
+	# TODO: Export adjacency matrix to CSV
+	adjMatrix = nx.adjacency_matrix(G).toarray()
+	graph_df = pd.DataFrame(adjMatrix)
+	graph_df.to_csv(path, header=False, index=False)
+
+	# TODO: Create custom Graph object
+	GraphObj = Graph.importTypedCSV(path, node_types)
+	GraphObj.layout = graphLayout
+	GraphObj.edges = G.number_of_edges()
+
+	# Export node types to file
+	with open(path+".types", "w") as output:
+		outStr = str(node_types)
+		outStr = outStr.replace("[", "")
+		outStr = outStr.replace("]", "")
+		output.write(outStr)
+
+	# Return custom graph object
+	return GraphObj
+
 
 # Generates a single complete graph with specified size
 def completeGraphGen(size, path="graphData/generatedCompleteGraph.csv", plotGraph=False):
