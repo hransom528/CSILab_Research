d449d43329a45bdbec40ecf4951ea8c59704106d
Harris Ransom <hransom528@gmail.com>
2025-05-11 22:32:00 -0400

    Finished M-ary experiments with Iris dataset

--- RandomWalk/IrisData.py
+++ RandomWalk/IrisData.py
@@ -42,12 +42,12 @@ feature_names = iris['feature_names']
 
 # Scale data to have mean 0 and variance 1 
 # which is important for convergence of the neural network
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+#scaler = StandardScaler()
+#X_scaled = scaler.fit_transform(X)
 
 # Split the data set into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=2)
+    X, y, test_size=0.2, random_state=2)
 
 # Visualize Iris data
 def visualizeDataset():
